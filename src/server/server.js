939f8d4cce88bb8d67cb05ab0598a263eee38328
g-horikoshi <g.horikoshi.216@gmail.com>
2023-11-13 15:07:14 +0900

    feat:post /reservations

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const knex = require('./knex');
 
-const reservationsMock = [{id: 0, artist : "mr.children", song: "HANABI"},{id: 1, artist : "B'z", song: "ultra soul"}];
-const artistsMock = [ {id: 0, artist: "mr.children"}, {id:1, artist: "B'z"}];
-const songsMock = [ {id: 0, artist: "mr.children", song:"HANABI"}, {id: 1, artist : "B'z", song: "ultra soul"}];
+const songTime = 10;
+const reservations = [{songId: 0, songName: "HANABI", artistName : "mr.children", },{songId: 1, songName: "ultra soul", artistName : "B'z"}];
 
 function setUpServer() {
     const app = express();
@@ -17,7 +16,26 @@ function setUpServer() {
 
     // reservations
     app.get('/reservations', (req, res) => {
-        res.status(200).json(reservationsMock);
+        res.json(reservations);
+    })
+
+    app.post('/reservations', (req, res) => {
+        req.body.forEach(song => {
+            knex.queryBuilder()
+            .select('songs.id as songId','songs.name as songName', 'artists.name as artistName' )
+            .from('songs')
+            .innerJoin('artists', 'artists.id', 'songs.artist_id')
+            .where('songs.id', song.songId)
+            .then(results => {
+                results.forEach(song => reservations.push(song));
+                res.json(reservations);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            })
+
+        })
     })
 
     // artists
@@ -27,6 +45,20 @@ function setUpServer() {
         .catch(err => {
             console.log(err);
             res.sendStatus(500);
+        })
+    });
+
+    app.post('/artists', (req,res) => {
+        req.body.forEach( obj => {
+            knex('artists')
+            .insert({name: obj.name})
+            .returning('id')
+            .then(id => {
+                res.json([{
+                    id: id,
+                    name: obj.name
+                }]);
+            })
         })
     });
 
@@ -43,20 +75,65 @@ function setUpServer() {
         })
     });
 
-    app.get('/:artistName/songs', (req, res) => {
-        const artistName = req.params.artistName;
-        console.log(artistName);
-        knex.queryBuilder()
-        .select('songs.id','songs.name as name', 'artists.name as artistName' )
-        .from('songs')
-        .innerJoin('artists', 'artists.id', 'songs.artist_id')
-        .where('artists.name', artistName)
-        .then(results => res.json(results))
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(500);
-        })
+    app.get('/songs', (req, res) => {
+        k
+    })
+
+    app.get('/:artistIdorName/songs', (req, res) => {
+        const artistIdorName = req.params.artistIdorName;
+        if (!isNaN(Number(artistIdorName))) {
+            // id
+            knex.queryBuilder()
+            .select('songs.id','songs.name as name', 'artists.name as artistName' )
+            .from('songs')
+            .innerJoin('artists', 'artists.id', 'songs.artist_id')
+            .where('artists.id', artistIdorName)
+            .then(results => res.json(results))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+             })
+        } else {
+            // name
+            knex.queryBuilder()
+            .select('songs.id','songs.name as name', 'artists.name as artistName' )
+            .from('songs')
+            .innerJoin('artists', 'artists.id', 'songs.artist_id')
+            .where('artists.name', artistIdorName)
+            .then(results => res.json(results))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            })
+
+        }
     });
+
+    app.post('/:artistId/songs', (req, res) => {
+        const artistId = req.params.artistId;
+        if (isNaN(Number(artistId))) {
+            res.sendStatus(400);
+        } else {
+            req.body.forEach( obj => {
+                knex('songs')
+                .insert({name: obj.name, artist_id: artistId})
+                .returning('id')
+                .then(id => {
+                    res.json([{
+                        id: id,
+                        name: obj.name, 
+                        artist_id: artistId
+                    }]);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.sendStatus(500);
+                })
+            })
+        }
+    });
+
+    
 
 
 
--- test/test.js
+++ test/test.js
@@ -22,18 +22,56 @@ describe('livedam server test', () => {
     it ('GET /artists' , async () => {
         const res = await chai.request(server).get('/artists');
         res.status.should.to.equal(200);
-        res.body.length.should.to.equal(3);
+        res.body.length.should.to.equal(5);
     });
 
     it ('GET /songs' , async () => {
         const res = await chai.request(server).get('/songs');
         res.status.should.to.equal(200);
-        res.body.length.should.to.equal(6);
+        res.body.length.should.to.equal(7);
     });
 
-    it ('GET /:artists/songs' , async () => {
+    it ('GET /:artistsName/songs' , async () => {
         const res = await chai.request(server).get('/mr.children/songs');
         res.status.should.to.equal(200);
         res.body.length.should.to.equal(3);
     });
+
+    it ('GET /:artistsId/songs' , async () => {
+        const res = await chai.request(server).get('/1/songs');
+        res.status.should.to.equal(200);
+        res.body.length.should.to.equal(3);
+    });
+
+    xit ('POST /artists' , async () => {
+        const newArtists = [
+            {
+                name: 'Larc en ciel',
+            }
+        ]
+        const res = await chai.request(server).post('/artists').send(newArtists);
+        res.status.should.to.equal(200);
+        res.body.length.should.to.equal(1);
+    });
+
+    xit ('POST /artistId/songs' , async () => {
+        const newSongs = [
+            {
+                name: '誘惑'
+            }
+        ]
+        const res = await chai.request(server).post('/4/songs').send(newSongs);
+        res.status.should.to.equal(200);
+        res.body.length.should.to.equal(1);
+    });
+
+    it ('POST /reservations',async () => {
+        const res = await  chai.request(server).post('/reservations').send([{songId: "7"}]);
+        res.status.should.to.equal(200);
+        res.body.length.should.to.equal(3);
+
+    })
+
+
+
 })
